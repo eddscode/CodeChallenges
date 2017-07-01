e3be7432fea5f565751a68b4522177673d8283b9
eddscode <sup@edwardhaigh.com>
2017-07-01 14:02:57 +0100

    Rosalind Problem 1 completed

---
+++ Rosalind/Counting_DNA_Nucleotides/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dnaString = "GGCTTCCTATGCGCGCTTGCTGGCATGTGGGCGTGACTTTCCTGAGATGGAGAAACTCATGCGTTAATACGCACGTTTAAGGAAAGACGCCGATAGGTACGGCGCTCGGTGCGATCAAGCCAGTTCCGTTGATTCAATCAGACACACGGGCGCACGGATCTCAAACGTACCGTAGAGGCTCGGGCACACCTGAATGGGGCGTGTTCAGGAAGTGACAGTCACCTGGTAACCCCAAAAAGTTAATGTGGTGCCTAGCATAGAACTGGGAACGGCGTGTAGTAGATGACAAACGGCGACTTGAGCAGACCACATGTCCTACGCTCACTTAGTCTGATTCGCCCCCTTCTAGATTATCTTCCTACAGCATGCTTGCGCACCGTATGGCGGAATCGTCACAGTTACAGTTACACTGTATGATCCCGTGGCATCGCCTAAAAAGGGCGCAGCCAATGGATCAAGAAAAAACCACGGACAGCAAAGTCGGATACACAAATGTATAAAGTCCTGTGGTGATCCAGCGCGGCTGGATTATTAAGATGGTTTCATGTGACAGATCTTACGTACCTCGCTATCCCCGACTGGCTCGTCCGGTAACGGCTGGATAACGGAACGACCGGCTCGGACAATTTACGACTCTCGTGTGAAAACCCATAATCGCTGGCAAATACACATTGACCAGTCCACCTATGCCCTGCTCAGCCGAGCTAGCTGTTGCTTTAGTAACCGTAAAGAACCAGGTCGGTATAAAGCCGGATTCCTGCGGTTCTGCGGGGTACTGATTACACGTCTTGGTCTACACTGATTGTACGGCGCTGAGAGGTGATTGGGGGGTGATGCTTGCGTGATTAATCTCCAG";
+            char[] dnaStringArray = dnaString.ToCharArray();
+
+            int A = 0, C = 0, G = 0, T = 0;
+
+            foreach (char c in dnaStringArray)
+            {
+                if (c == 'A')
+                {
+                    A += 1;
+                }
+                else if (c == 'C')
+                {
+                    C += 1;
+                }
+                else if (c == 'G')
+                {
+                    G += 1;
+                }
+                else if (c == 'T')
+                {
+                    T += 1;
+                }
+            }
+
+            Console.WriteLine($"{A} {C} {G} {T}");
+            Console.ReadKey();
+        }
+    }
+}
