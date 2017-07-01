85b35dadbf3b2688483817f4ea2f3776d6f60d16
eddscode <sup@edwardhaigh.com>
2017-07-01 14:40:23 +0100

    Completed Rosalind problem 3

---
+++ Rosalind/Complementing_a_Strand_of_DNA/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public static class Program
+    {
+        static string Reverse(string text)
+        // The reverse complement of a DNA string 's' is the string 'sc'
+        // formed by reversing the symvbols of 's'.
+        {
+            char[] array = text.ToCharArray();
+            Array.Reverse(array);
+            return new String(array);
+        }
+
+        static string ConvertToDNAComplement(string text)
+        // In DNA strings, symbols 'A' and 'T'; are complements of each 
+        // other, as are 'C' and 'G'.
+        {
+            char[] array = text.ToCharArray();
+            string complementedString = "";
+
+            foreach (char c in array)
+            {
+                if (c == 'A')
+                {
+                    complementedString += 'T';
+                }
+                else if (c == 'T')
+                {
+                    complementedString += 'A';
+                }
+                else if (c == 'C')
+                {
+                    complementedString += 'G';
+                }
+                else if (c == 'G')
+                {
+                    complementedString += 'C';
+                }
+            }
+
+            return complementedString;
+        }
+
+        static void Main(string[] args)
+        // Test dnaString = 'AAAACCCGGT', expect 'ACCGGGTTTT'
+        {
+            string dnaString = "TCGTCTTGCGCGTTTAAGGCGCGGAAAGTATTGTCCCGACCCTCGATGGACCCTGATATTAACCTCTGGGGCACTTTCTCCGAATCAGTTCATCATCTATACTTCCTTGTCTACCTAGTCATCTCTCGGTCTCCCAACGAGACAGTGGATCCCTGACTCAGTTTTTCTCAAACGAAGATGAATAAACAGCCGGCTCTGCTGTTGGCTAGGGCATGACGAATGGACGCCCACGTCTCCTGACCATGTTATGATTGAGAGACATGCGCGTGGCAAATCTCACTAAAGGCTACCTAGGAACTTAATGTGTAACCGTCCACTGTGTTAGACTCGATCAGCCGAACGGTAAGGTGGTGCACCCAAGCAACTCTGCGTGAGCAAGGCGTTCTCCGGCTCTTTCGGATCAAGTAATAGGCAGTTGGATAATGCGCCTAGCGCGGGAGAACTATGGTGAGGCCCTCACCTTCCGTGCGTCACTGTGATTCGTAATGGGATAGCATCGCACTGGATTATGGTCTACTGAGTATAATATCTCCTCCCTAAACAACCCTAGCTCACGCTGTCAGGCCCCCTGTATTGACTCATCACGACACCACAGCCTCGATTTGCATTGACCATTAGAACTTTAAGAATTGCTGGCACCACCGGGTGCGTAGCCTTCCCAGGCTGAGTTCAATTACTGTCACATGGGGTTCAGGCCCGTTTAAACTGAGTTTAATGGATTGACCACTCAGTGTCAACTTCTCGCTTTCTAAGATTGGGGTATGATAGCAGTGGTATTATTGCCAGTATCGGACGCCCGAGGACCGCGACGCTGGACGACTTACGGATTTAGTCTGCTTACT";
+            string reversedString = Reverse(dnaString);
+            string complementedString = ConvertToDNAComplement(reversedString);
+            Console.WriteLine(complementedString);
+            Console.ReadKey();
+        }
+    }
+}
